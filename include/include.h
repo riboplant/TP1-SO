830c0c0e12e5d4fdf0e3d5f63d60681fc4d85c02
Ignacio Maruottolo <ignaciomaruottolo@gmail.com>
2024-08-22 15:10:25 -0300

    Implemented slave and partially implemented app

---
+++ include/include.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/file.h>
+#include <time.h>
+#include <sys/select.h>
+#include <fcntl.h>  // Para open
+
+#define MAX_PATH_LENGTH 64
+#define MAX_FILE_COUNT 256
+#define SLAVE_COUNT 5
+
+typedef char* input;
+
+typedef struct output
+{
+    pid_t pid;
+    char * md5;
+    char * file_name
+} output;
