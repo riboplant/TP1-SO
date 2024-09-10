c70eb96f64d051cbd0b1a6a6f1be3d7c1af89f82
conradohillar <hillarconrado@gmail.com>
2024-09-10 13:23:06 -0300

    added logic to slave_count calculation, grouped data in structs

--- src/app.c
+++ src/app.c
@@ -2,44 +2,49 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 
 #include "../include/include.h"
+#include <stdlib.h>
 
-void create_slaves(int * pipes[][2]);
-void file_handler(int argc, char * argv[], int * pipes[][2]);
+typedef struct slave {
+    int pipeIn[2];
+    int pipeOut[2];
+    pid_t pid;
+} slaveT;
+
+slaveT * create_slaves();
+void file_handler(int argc, char * argv[], slaveT * pipes);
 int check_path(char * path, struct stat fileStat);
-int cycle_pipes(int argc, char * argv[], int * pipes[][2],  struct stat fileStat);
-void get_results(int * pipes[][2]);
-int send_to_slave(char * argv[], int fd, int * pipes[][2], struct stat fileStat);
+int cycle_pipes(int argc, char * argv[], slaveT * pipes,  struct stat fileStat);
+void get_results(slaveT * pipes);
+int send_N_to_slave(char * argv[], int fd, slaveT * pipes, struct stat fileStat, int n);
+int send_to_slave(char * argv[], int fd, slaveT * pipes, struct stat fileStat);
 
-static int get_shared_block(char* filename, int size);
-void * attach_memory_block(char* filename, int size);
-int detach_memory_block(char* block);
-int destroy_memory_block(char* filename);
-void write_shmem(char * data, int data_size);
+// static int get_shared_block(char* filename, int size);
+// void * attach_memory_block(char* filename, int size);
+// int detach_memory_block(char* block);
+// int destroy_memory_block(char* filename);
+// void write_shmem(char * data, int data_size);
 
 static int file_list_iter = 1;
-
-char * shmblock;
-int block_size;
-int used_space = 0;
+static int slave_count;
 
 sem_t * sem_prod;
 sem_t * sem_cons;
 
+typedef struct shmemBlock {
+    char * shmblock;
+    int block_size;
+    int used_space;
+} shmemBlockT; 
+
 int main(int argc, char * argv[]) {
-    // "in" and "out" are in reference to the slaves
-    int pipe1_in[2], pipe1_out[2], 
-        pipe2_in[2], pipe2_out[2], 
-        pipe3_in[2], pipe3_out[2], 
-        pipe4_in[2], pipe4_out[2], 
-        pipe5_in[2], pipe5_out[2];
+    int ratio = ((argc - 1) / SLAVE_FACTOR);
+    
+    slave_count = ((ratio > 0) ? ratio : ((argc - 1) / PIPE_FILE_COUNT > 0 ? (argc - 1) / PIPE_FILE_COUNT : 1) );
+    slave_count = (slave_count > MAX_SLAVE_COUNT ? MAX_SLAVE_COUNT : slave_count); 
 
-    int * pipes[SLAVE_COUNT][2] = {{pipe1_in, pipe1_out},
-                                   {pipe2_in, pipe2_out},
-                                   {pipe3_in, pipe3_out},
-                                   {pipe4_in, pipe4_out},
-                                   {pipe5_in, pipe5_out}};
+    slaveT * pipes = create_slaves();
 
-    create_slaves(pipes);
+    
 
     // sem_unlink(SEM_PRODUCER_FNAME);
     // sem_unlink(SEM_CONSUMER_FNAME);
@@ -71,9 +76,9 @@ int main(int argc, char * argv[]) {
 
 
     // Close entry pipe writing fd when finished parsing
-    for(int i=0; i<SLAVE_COUNT; i++){
-        close(pipes[i][0][1]);
-        close(pipes[i][1][0]);
+    for(int i = 0; i < slave_count; i++){
+        close(pipes[i].pipeIn[1]);
+        close(pipes[i].pipeOut[0]);
     } 
 
 
@@ -86,7 +91,7 @@ int main(int argc, char * argv[]) {
     // sem_close(sem_cons);
     // detach_memory_block(shmblock);
     // destroy_memory_block(FILENAME);
-
+    free(pipes);
     exit(0);
 }
 
@@ -94,17 +99,18 @@ int main(int argc, char * argv[]) {
 /*
 Creates a number of slaves specified by SLAVE_COUNT and returns an array of the slaves' PIDs
 */
-void create_slaves(int * pipes[][2]) {
-    pid_t cpid;
-    for(int i = 0; i < SLAVE_COUNT; i++) {
-        // Create pipes
-        for(int j = 0; j < 2; j++) {
-            if(pipe(pipes[i][j]) == -1) {
-                perror("pipe failed");
-                exit(EXIT_FAILURE);
-            }
-        }
+slaveT * create_slaves() {
 
+    slaveT * pipes = malloc(sizeof(slaveT) * slave_count);
+    
+    pid_t cpid;
+    for(int i = 0; i < slave_count; i++) {
+        // Create pipes
+        if(pipe(pipes[i].pipeIn) == -1 || pipe(pipes[i].pipeOut) == -1) {
+            perror("pipe failed");
+            exit(EXIT_FAILURE);
+        }
+        
         cpid = fork();
         if(cpid == -1) {
             perror("fork error");
@@ -112,16 +118,16 @@ void create_slaves(int * pipes[][2]) {
         }
         else if(cpid == 0) {
             //Close any other fd 
-            for(int j=0; j <= i; j++){
-                close(pipes[j][1][0]); 
-                close(pipes[j][0][1]);
+            for(int j = 0; j <= i; j++){
+                close(pipes[j].pipeOut[0]); 
+                close(pipes[j].pipeIn[1]);
             }
             
-            dup2(pipes[i][0][0], STDIN_FILENO); // Redirect stdin to entry pipe input
-            close(pipes[i][0][0]); // Close original entry pipe input
+            dup2(pipes[i].pipeIn[0], STDIN_FILENO); // Redirect stdin to entry pipe input
+            close(pipes[i].pipeIn[0]); // Close original entry pipe input
 
-            dup2(pipes[i][1][1], STDOUT_FILENO); // Redirect stdout to exit pipe output
-            close(pipes[i][1][1]); // Close original entry pipe output
+            dup2(pipes[i].pipeOut[1], STDOUT_FILENO); // Redirect stdout to exit pipe output
+            close(pipes[i].pipeOut[1]); // Close original entry pipe output
 
             char * argv[] = {"slave"};
             char * envp[] = {NULL};
@@ -130,49 +136,71 @@ void create_slaves(int * pipes[][2]) {
             exit(1);
         }
 
-            close(pipes[i][0][0]); // Close entry pipe reading fd for app
-            close(pipes[i][1][1]); // Close exit pipe writing fd for app
+            close(pipes[i].pipeIn[0]); // Close entry pipe reading fd for app
+            close(pipes[i].pipeOut[1]); // Close exit pipe writing fd for app
+            pipes[i].pid = getpid();    
     }
-    return;
+    return pipes;
 }
 
-void file_handler(int argc, char * argv[], int * pipes[][2]) {
-    int min_files = ((SLAVE_COUNT < (argc - 1)) ? SLAVE_COUNT : (argc - 1));
+void file_handler(int argc, char * argv[], slaveT * pipes) {
+    int min_files = ((slave_count < (argc - 1)) ? slave_count : (argc - 1));
     struct stat fileStat;
 
     //primera pasada, le paso PIPE_FILE_COUNT a cada slave para arrancar
     //TODO: se puede modularizar haciendo que cycle_pipes reciba cuantos archivos pasarle a cada pipe
-    for(int i = 0; i < min_files; i++ ) {
-        for(int j = 0; j < PIPE_FILE_COUNT && file_list_iter <= (argc - 1); j++) {
-            send_to_slave(argv, pipes[i][0][1], pipes, fileStat);   
-        }
+    for(int i = 0; i < min_files; i++){
+        send_N_to_slave(argv, pipes[i].pipeIn[1], pipes, fileStat, PIPE_FILE_COUNT);
     }
+
     while(file_list_iter < argc) {
         cycle_pipes(argc, argv, pipes, fileStat);
     }
 }
 
+
+// int send_to_slave(char * argv[], int fd, slaveT * pipes, struct stat fileStat) {
+
+//   //  sem_wait(sem_prod); // Wait for view.c to consume from buffer
+
+//     int c, ans = -1;
+//     if((c = check_path(argv[file_list_iter], fileStat)) == -1){
+//                 perror("Invalid path");
+//                 exit(EXIT_FAILURE);
+//         } else if(c == 0){
+//             if((ans = write(fd, argv[file_list_iter], strlen(argv[file_list_iter]))) == -1){
+//                 perror("Write failed: ");
+//                 exit(EXIT_FAILURE);
+//             }
+//             else {
+//                 get_results(pipes);
+//             }
+//         }
+//     file_list_iter++;
+//     return ans;
+// }
 /*
-    Returns -1 on write error or the number written otherwise
+    Returns -1 on write error or the number of slaves otherwise
 */
-int send_to_slave(char * argv[], int fd, int * pipes[][2], struct stat fileStat) {
-
-  //  sem_wait(sem_prod); // Wait for view.c to consume from buffer
-
-    int c, ans = -1;
-    if((c = check_path(argv[file_list_iter], fileStat)) == -1){
-                perror("Invalid path");
-                exit(EXIT_FAILURE);
-        } else if(c == 0){
-            if((ans = write(fd, argv[file_list_iter], strlen(argv[file_list_iter]))) == -1){
-                perror("Write failed: ");
-                exit(EXIT_FAILURE);
+int send_N_to_slave(char * argv[], int fd, slaveT * pipes, struct stat fileStat, int n) {
+    printf("fd -> %d\n", fd);
+    int ans = -1;
+    for(int i = 0; i < n; i++) {
+        int c = -1;
+        if((c = check_path(argv[file_list_iter], fileStat)) == -1){
+                    perror("Invalid path");
+                    exit(EXIT_FAILURE);
+            } else if(c == 0){
+                if((ans = write(fd, argv[file_list_iter], strlen(argv[file_list_iter]))) == -1){
+                    perror("Write failed: ");
+                    exit(EXIT_FAILURE);
+                }
+                else {
+                    get_results(pipes);
+                }
             }
-            else {
-                get_results(pipes);
-            }
-        }
-    file_list_iter++;
+        file_list_iter++;           
+    }
     return ans;
 }
 
@@ -198,7 +226,7 @@ int check_path(char * path, struct stat fileStat){
     Cycles through all slave pipes and reads their ouptut, if there is one available. 
     Passes file paths to every slave's pipe.
 */
-int cycle_pipes(int argc, char * argv[], int * pipes[][2], struct stat fileStat) {
+int cycle_pipes(int argc, char * argv[], slaveT * pipes, struct stat fileStat) {
 
   //  sem_wait(sem_prod); // Wait for view.c to consume from buffer
 
@@ -206,17 +234,17 @@ int cycle_pipes(int argc, char * argv[], int * pipes[][2], struct stat fileStat)
     int max_fd = 0;
 
     // Buscamos el descriptor más grande
-    for(int i = 0; i < SLAVE_COUNT; i++) {
-        if(max_fd < pipes[i][0][1]) {
-            max_fd = pipes[i][0][1];
+    for(int i = 0; i < slave_count; i++) {
+        if(max_fd < pipes[i].pipeIn[1]) {
+            max_fd = pipes[i].pipeIn[1];
         }
     }
     max_fd += 1;
 
     FD_ZERO(&writefds);  // Limpiar el conjunto de descriptores de escritura
     
-    for(int i = 0; i < SLAVE_COUNT; i++) {
-        FD_SET(pipes[i][0][1], &writefds);
+    for(int i = 0; i < slave_count; i++) {
+        FD_SET(pipes[i].pipeIn[1], &writefds);
     }
 
     // Esperar hasta que alguno de los pipes este listo para lectura
@@ -227,32 +255,32 @@ int cycle_pipes(int argc, char * argv[], int * pipes[][2], struct stat fileStat)
         exit(EXIT_FAILURE);
     }
 
-    for(int i = 0; i < SLAVE_COUNT && (file_list_iter < argc); i++) {
-        if(FD_ISSET(pipes[i][0][1], &writefds)){
-            send_to_slave(argv, pipes[i][0][1], pipes, fileStat);      //Envío el archivo al fd de escritura del pipe de entrada
+    for(int i = 0; i < slave_count && (file_list_iter < argc); i++) {
+        if(FD_ISSET(pipes[i].pipeIn[1], &writefds)){
+            send_N_to_slave(argv, pipes[i].pipeIn[1], pipes, fileStat, 1);      //Envío el archivo al fd de escritura del pipe de entrada
         }
     }
     
     return(0);
 }
 
-void get_results(int * pipes[][2]){
+void get_results(slaveT * pipes){
 
     fd_set readfds;
     int max_fd = 0;
 
     // Buscamos el descriptor más grande
-    for(int i = 0; i < SLAVE_COUNT; i++) {
-        if(max_fd < pipes[i][1][0]) {
-            max_fd = pipes[i][1][0];
+    for(int i = 0; i < slave_count; i++) {
+        if(max_fd < pipes[i].pipeOut[0]) {
+            max_fd = pipes[i].pipeOut[0];
         }
     }
     max_fd += 1;
 
     FD_ZERO(&readfds);
 
-    for(int i = 0; i < SLAVE_COUNT; i++) {
-        FD_SET(pipes[i][1][0], &readfds);
+    for(int i = 0; i < slave_count; i++) {
+        FD_SET(pipes[i].pipeOut[0], &readfds);
     }
 
     int activity = select(max_fd, &readfds, NULL, NULL, NULL);
@@ -262,14 +290,14 @@ void get_results(int * pipes[][2]){
         exit(EXIT_FAILURE);
     }
 
-    for(int i = 0; i < SLAVE_COUNT; i++) {
+    for(int i = 0; i < slave_count; i++) {
 
-        if(FD_ISSET(pipes[i][1][0], &readfds)) {
+        if(FD_ISSET(pipes[i].pipeOut[0], &readfds)) {
             char buffer[MAX_PATH_LENGTH + MD5_LENGTH + PID_LENGTH + 3]; // 3 extra spaces for spaces and '\0'
             int count;
             // char md5_hash[MD5_LENGTH]; // Buffer para el hash MD5 (32 caracteres + 1 para '\0')
             // char filename[MAX_PATH_LENGTH]; // Buffer para el nombre del archivo
-            if((count = read(pipes[i][1][0], buffer, sizeof(buffer)-1)) == -1) {
+            if((count = read(pipes[i].pipeOut[0], buffer, sizeof(buffer)-1)) == -1) {
                 perror("Read error");
                 exit(EXIT_FAILURE);
             } 
@@ -291,60 +319,60 @@ void get_results(int * pipes[][2]){
 
 // A partir de aca estan las funciones auxiliares de memshare
 
-static int get_shared_block(char* filename, int size){
-    FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        perror("Failed to create temp file");
-        exit(EXIT_FAILURE);
-    }
-    fclose(file);
+// static int get_shared_block(char* filename, int size){
+//     FILE *file = fopen(filename, "w");
+//     if (file == NULL) {
+//         perror("Failed to create temp file");
+//         exit(EXIT_FAILURE);
+//     }
+//     fclose(file);
 
-    // Request a key, the key is linked to a filename, so that other programs can access it.
-    key_t key= ftok(filename, 0);
-    if(key == IPC_RESULT_ERROR) return IPC_RESULT_ERROR;
+//     // Request a key, the key is linked to a filename, so that other programs can access it.
+//     key_t key= ftok(filename, 0);
+//     if(key == IPC_RESULT_ERROR) return IPC_RESULT_ERROR;
 
-    unlink(filename);
+//     unlink(filename);
 
-    //Get shared block --- create it if it does not exist
-    return shmget(key, size, 0644 | IPC_CREAT);
-}
+//     //Get shared block --- create it if it does not exist
+//     return shmget(key, size, 0644 | IPC_CREAT);
+// }
 
-void * attach_memory_block(char* filename, int size){
-    int shared_block_id = get_shared_block(filename, size);
-    char* result;
+// void * attach_memory_block(char* filename, int size){
+//     int shared_block_id = get_shared_block(filename, size);
+//     char* result;
 
-    if(shared_block_id == IPC_RESULT_ERROR) {
-        return NULL;
-    }
+//     if(shared_block_id == IPC_RESULT_ERROR) {
+//         return NULL;
+//     }
 
-    // Map the shared block into this process's memory and give me a pointer to it
-    result = shmat(shared_block_id, NULL, 0);
-    if(result == (void *)IPC_RESULT_ERROR){
-        return NULL;
-    }
-    return result;
-}
+//     // Map the shared block into this process's memory and give me a pointer to it
+//     result = shmat(shared_block_id, NULL, 0);
+//     if(result == (void *)IPC_RESULT_ERROR){
+//         return NULL;
+//     }
+//     return result;
+// }
 
-int detach_memory_block(char * block){
-    return (shmdt(block) != IPC_RESULT_ERROR);
-}
+// int detach_memory_block(char * block){
+//     return (shmdt(block) != IPC_RESULT_ERROR);
+// }
 
-int destroy_memory_block(char * filename){
-    int shared_blok_id = get_shared_block(filename, 0);
+// int destroy_memory_block(char * filename){
+//     int shared_blok_id = get_shared_block(filename, 0);
 
-    if(shared_blok_id == IPC_RESULT_ERROR) return -1;
+//     if(shared_blok_id == IPC_RESULT_ERROR) return -1;
 
-    return (shmctl(shared_blok_id, IPC_RMID, NULL) != IPC_RESULT_ERROR);
-}
+//     return (shmctl(shared_blok_id, IPC_RMID, NULL) != IPC_RESULT_ERROR);
+// }
 
-void write_shmem(char * data, int data_size){
-    // Verificar si hay suficiente espacio en el bloque
-    if (used_space + data_size > block_size) {
-        printf("No hay suficiente espacio en la memoria compartida.\n");
-        return;
-    }
+// void write_shmem(char * data, int data_size){
+//     // Verificar si hay suficiente espacio en el bloque
+//     if (used_space + data_size > block_size) {
+//         printf("No hay suficiente espacio en la memoria compartida.\n");
+//         return;
+//     }
 
-    // Escribir datos en la memoria compartida
-    memcpy(shmblock + used_space, data, data_size);
-    used_space += data_size;  // Actualizar el espacio utilizado
-}
+//     // Escribir datos en la memoria compartida
+//     memcpy(shmblock + used_space, data, data_size);
+//     used_space += data_size;  // Actualizar el espacio utilizado
+// }
